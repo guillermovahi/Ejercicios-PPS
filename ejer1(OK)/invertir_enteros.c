5b8a6a43067338af7c2b57bcef6035eae7453df7
Guillermo <guillermovahi@gmail.com>
2021-11-18 19:17:49 +0100

    ejer 2 OK

--- ejer1(OK)/invertir_enteros.c
+++ ejer1(OK)/invertir_enteros.c
@@ -6,13 +6,13 @@ int main(void)
     int *array_of_ints;
     int i;
 	int x;
+	
 	array_of_ints = malloc(sizeof(int));
+	
 	i = 0;
 	while(scanf("%i", &x))
-	{
-		array_of_ints[i] = x;
-		i++; 
-	}
+		array_of_ints[i++] = x;
+
 	while(--i>=0)
 		fprintf(stdout,"%i\n",array_of_ints[i]);
 		
--- ejer2/ejer2.c
+++ ejer2/ejer2.c
@@ -1,10 +1,96 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void assign_size(int m, int n, int p);
+void fill_matrix(int m, int n, int p, int mat1[m][n], int mat2[n][p]);
+void mult_matrix(int m, int n, int p, int mat1[m][n], int mat2[n][p]);
 
 int main(int argc, char *argv[])
 {
-	FILE *archivo;
-
-	archivo = fopen(argv[1], "r");
-
+	int m; // m filas de la primera matriz
+	int n; // n columnas de la primera matriz y n filas de la segunda matriz
+	int p; // p columnas  segunda matriz
 	
+	// Asignamos las dimensiones de la matriz
+	assign_size(m,n,p);
+
+	return 0;
+}
+
+void assign_size(int m, int n, int p)
+{
+	int i; // contador
+	int num; 
+
+	i = -1;
+	while(++i < 3 && scanf("%i", &num))
+	{
+		if (i == 0)
+			m = num;
+		else if (i == 1)
+			n = num;
+		else
+			p = num;	
+	}
+
+
+	int mat1[m][n];
+	int mat2[n][p];
+	
+	fill_matrix(m, n, p, mat1, mat2);
+	mult_matrix(m, n, p, mat1, mat2);
+
+}
+
+void fill_matrix(int m, int n, int p, int mat1[m][n], int mat2[n][p])
+{
+	int i;
+	int j;
+	char string[1000];
+
+	//Rellenar Primera Matriz
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j < n; j++){
+			scanf("%s", &string);
+			mat1[i][j] = atoi(string);
+		}
+	}
+
+	//Rellenar Segunda Matriz
+	for(i = 0; i < n; i++)
+	{
+		for(j = 0; j < p; j++){
+			scanf("%s", &string);
+			mat2[i][j] = atoi(string);
+		}
+	}
+
+}
+
+void mult_matrix(int m, int n, int p, int mat1[m][n], int mat2[n][p])
+{
+	int	i, j, k;
+	int mat_res[m][p];
+
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j < p; j++)
+			mat_res[i][j] = 0;
+	}	
+
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j < p; j++){
+			for(k = 0; k < n; k++)
+				mat_res[i][j] += mat1[i][k] * mat2[k][j];
+		}
+	}
+
+	for(i = 0; i < m; i++)
+	{
+		for(j = 0; j < p; j++)
+			printf("%i ", mat_res[i][j]);
+		printf("\n");
+	}	
 }
